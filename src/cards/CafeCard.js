2d31b66c414dab61771a48e5b462cf4bec411ca5
Anna-Katharina Drewes <annak.drewes@googlemail.com>
2019-05-22 12:58:57 +0200

    Add card styling

--- src/cards/CafeCard.js
+++ src/cards/CafeCard.js
@@ -3,19 +3,22 @@ import styled from 'styled-components'
 
 const CardGrid = styled.section`
     grid-column: 2 / 3;
+    display: grid;
+    grid-template-columns: 25px auto 25px;
     height: 500px;
     position: relative;
 `
 
 const Image = styled.img`
-    position: absolute;
     left: 25px;
     height: 250px;
     width: 250px;
+    object-fit: cover;
+    position: absolute;
     z-index: 2;
 `
 
-const CardInfo = styled.div`
+const CardBackgroundColor = styled.div`
     height: 450px;
     width: 100%;
     background: #77A6A1;
@@ -23,24 +26,36 @@ const CardInfo = styled.div`
     top: 50px;
 `
 
+const CafeInfo = styled.div`
+    grid-column: 2 / 3;
+    position: absolute;
+    top: 250px;
+    text-align: center;
+    color: #E4E9F2;
+`
+
 const CafeTitle = styled.h2`
     font-family: Monaco;
+    font-weight: 400;
     font-size: 16px;
+    letter-spacing: 1.74px;
 `
 
 const CafeAddress = styled.p`
     font-family: Tahoma;
     font-size: 14px;
+    letter-spacing: 0.64px;
 `
 
-export default function CafeCard({src, alt, title, address}) {
+export default function CafeCard({src, alt, title, street, district}) {
     return (
         <CardGrid>
             <Image src={src} alt={alt} />
-            <CardInfo>
+            <CardBackgroundColor />
+            <CafeInfo>
                 <CafeTitle>{title}</CafeTitle>
-                <CafeAddress>{address}</CafeAddress>
-            </CardInfo>
+                <CafeAddress>{street}<br/>{district}</CafeAddress>
+            </CafeInfo>
         </CardGrid>
     )
 }
