060cbb1f514c0ef6a4cf7ac9fde1e25794309a63
Anna-Katharina Drewes <annak.drewes@googlemail.com>
2019-05-27 17:15:18 +0200

    Add setting a rating

--- src/cards/DynamicRating.js
+++ src/cards/DynamicRating.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import WorkIcon from '../images/WorkAtmosphereY.png'
 import WorkIconDisabled from '../images/WorkAtmosphereN.png'
@@ -30,14 +30,43 @@ width: 90%;
 align-self: center;
 `
 
-
 export default function DynamicRating() {
+
+    const [activeWorkIndex, setActiveWorkIndex] = useState(null)
+    const [activeWlanIndex, setActiveWlanIndex] = useState(null)
+    const [activeCupIndex, setActiveCupIndex] = useState(null)
     
+    const iconsArray = new Array(5).fill('');
+    
+    const handleToggleWorkIcon = (index1) => {
+        setActiveWorkIndex(index1);
+    }
+
+    const handleToggleWlanIcon = (index2) => {
+        setActiveWlanIndex(index2);
+    }
+
+    const handleToggleCupIcon = (index3) => {
+        setActiveCupIndex(index3);
+    }
+
     return (
         <>
-            <SingleRating>{new Array(5).fill('').map(() => <WorkIconStyle src={WorkIconDisabled} /> )}</SingleRating>
-            <SingleRating>{new Array(5).fill('').map(() => <WlanIconStyle src={WlanIconDisabled} /> )}</SingleRating>
-            <SingleRating>{new Array(5).fill('').map(() => <CupIconStyle src={CupIconDisabled} /> )}</SingleRating>
+            <SingleRating>
+                {iconsArray.map((icon, index1) => <WorkIconStyle onClick={() => handleToggleWorkIcon(index1)} src={index1 <= activeWorkIndex ? WorkIcon : WorkIconDisabled} /> )}
+            </SingleRating>
+
+            <SingleRating>
+                {iconsArray.map((icon, index2) => <WlanIconStyle onClick={() => handleToggleWlanIcon(index2)} src={index2 <= activeWlanIndex ? WlanIcon : WlanIconDisabled} /> )}
+            </SingleRating>
+
+            <SingleRating>
+                {iconsArray.map((icon, index3) => <CupIconStyle onClick={() => handleToggleCupIcon(index3)} src={index3 <= activeCupIndex ? CupIcon : CupIconDisabled} /> )}
+            </SingleRating>
         </>
     )
 }
+
+// <Rating label="Arbeitsklima" active={true} icon={WorkIcon} activeIcon={WorkIconActive} activeIndex={2} onIconClick={index => console.log(index)}/>
+// <Rating icon={Wlan} activeIcon={WorkIconActive} />
+// <Rating icon={WorkIcon} activeIcon={WorkIconActive} /> 
